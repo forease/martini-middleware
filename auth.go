36956622e4ee739e3fbbb41bfcaa8e40b79dee62
Jonsen Yang <im16hot@gmail.com>
2015-04-28 10:51:04 +0800

    change AuthRequest()

--- auth.go
+++ auth.go
@@ -2,42 +2,45 @@ package middleware
 
 import (
 	"github.com/go-martini/martini"
-	"reflect"
+	"net/http"
 )
 
-const (
-	SignInRequired = 100
-)
+//
+//  	urls := []string{"/login", "/users", "/firewall"}
+//	    m.Use(middleware.AuthRequest(urls...))
+//
+func AuthRequest(disurls ...string) martini.Handler {
+	return func(ctx martini.Context, c *Context, r *http.Request) {
+		var (
+			match bool
+		)
 
-func AuthRequest(req interface{}, redirect string) martini.Handler {
-	return func(c *Context) {
-		if redirect == "" {
-			redirect = "/login"
-		}
-
-		switch req {
-		case SignInRequired:
-			if user := c.S.Get("Authorized"); user != nil {
-				// pass
+		if authored := c.S.Get("Authorized"); authored != nil {
+			if u := c.S.Get("AuthInfo"); u != nil {
+				c.Set("AuthData", u)
+				ctx.Next()
 				return
 			}
-			c.Redirect(redirect)
-			return
-		default:
-			if user := c.S.Get("Authorized"); user != nil {
-				// pass
-				return
-				if reflect.TypeOf(req).Kind() == reflect.Int {
-
-					c.HTML(403, "error/403", c)
-					return
-				}
-			} else {
-				c.Redirect(redirect)
-				return
-			}
-			c.HTML(403, "error/403", c)
-			return
 		}
+
+		urlLen := len(disurls)
+
+		if urlLen < 1 {
+			disurls[0] = "/login"
+			urlLen = 1
+		}
+
+		for i := 0; i < urlLen; i++ {
+			if r.RequestURI == disurls[i] {
+				match = true
+				break
+			}
+		}
+
+		if !match && r.RequestURI != "/login" && r.RequestURI != "/users/logined" {
+			c.Redirect(disurls[0])
+		}
+
+		ctx.Next()
 	}
 }
